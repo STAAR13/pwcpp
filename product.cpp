7f30b224cbb2b4fcdf764d71b76a66d9c16c9d63
STAAR13 <112777730+STAAR13@users.noreply.github.com>
2023-03-21 13:24:22 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x,y,product;
+    cout<<"x :";
+    cin>>x;
+    cout<<"y :";
+    cin>>y;
+
+    product=x*y;
+    cout<<"product of "<<x<<" and "<<y<<" is "<<product<<endl; 
+
+    return 0;
+
+}
